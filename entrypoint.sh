cb9198677af7ad8f0fb393dd758ab071d43dee08
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-06-13 13:34:32 +0200

    qbee-cli login

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,4 @@ api_host=$3
 
 echo "init login $login and password $password on $api_host"
 
-successful_status_code='200'
-
-output=$(curl --request POST -sL --url "https://$api_host/api/v2/login" \
-        --header "Content-Type: application/json" \
-        -d "{\"email\":\"$login\",\"password\":\"$password\"}" \
-        -w "\n{\"http_code\":%{http_code}}\n")
-
-http_code=$(echo $output | jq -cs | jq -r '.[1].http_code')
-
-if [ "$http_code" != "$successful_status_code" ]; then
-    echo "http_code was - $http_code"
-    exit 1
-else
-    echo 'successful authentication'
-fi
-
-tokenValue=$(echo $output | jq -cs | jq -r '.[0].token')
-
-echo "token=$tokenValue" >> $GITHUB_OUTPUT
-
+qbee-cli login -u $login -p $password -b https://$api_host
